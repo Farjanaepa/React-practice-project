0af0138f4c9ad8aa839857f3e9e8030ba4ba7e52
Farjana Epa <farjanaepa@gmail.com>
2025-09-21 12:35:53 +0600

    react practice project module 37 count set

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import Counter from './Counter'
+import './App.css'
+
+
+function App() {
+
+  function handleClick(){
+    alert('i am Clicked')
+  }
+
+  const handleClick3= () => {
+    alert('clicked 3')
+  }
+
+  const handleAdd5 = (num) => {
+    const newNum = num + 5;
+    alert(newNum);
+  }
+
+  return (
+    <>
+      <h1>Hellow</h1>
+      <Counter></Counter>
+      <button onClick={handleClick}>Click me</button>
+      <button onClick={function handleClick2(){
+        alert('clicked 2')
+      }}>Click me 2</button>
+      <button onClick={handleClick3}>Click me 3</button>
+      <button onClick={() => alert('click 4')}>Click me 4</button>
+      <button onClick={() => handleAdd5(7)}>Click add 5</button>
+    </>
+  )
+}
+
+export default App
